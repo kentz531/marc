13dc1e646abb19cbd410e7fac321ec8b2af88583
Marc Philippe de Villeres <mpdevilleres@gmail.com>
2018-10-27 09:28:12 +0400

    added 27-OCT-2018 Lecture

--- Chapter-01/exercise01.py
+++ Chapter-01/exercise01.py
@@ -5,34 +5,30 @@ print("Budget Calculator . . .")
 
 name = input("What's your Name? ")
 
-budget = input("What is your total Budget for a month? " )
-count_apple = input("How many apple/s you consume in a month? ")
-count_orange = input("How many orange/s you consume in a month? ")
+budget = int(input("What is your total Budget for a month? " ))
+apple_count = int(input("How many apple/s you consume in a month? "))
+apple_cost = int(input("How many apple/s you consume in a month? "))
+orange_count = int(input("How many orange/s you consume in a month? "))
+orange_cost = int(input("How many orange/s you consume in a month? "))
 
 
 # ------------------------------------------
 # PROCESS
 # ------------------------------------------
-apple_per_piece = 10
-orange_per_piece = 6
+apple_total_cost = int(apple_count) * apple_cost
+orange_total_cost = int(orange_count) * orange_cost
+percent_cost_apple = (apple_cost / int(budget)) * 100
+percent_cost_orange = (orange_cost / int(budget)) * 100
 
-cost_apple = int(count_apple) * apple_per_piece
-cost_orange = int(count_orange) * orange_per_piece
-remaining_budget = int(budget) - cost_apple - cost_orange
-
-percent_cost_apple = (cost_apple / int(budget)) * 100
-percent_cost_orange = (cost_orange / int(budget)) * 100
+remaining_budget = int(budget) - apple_total_cost - orange_total_cost
 percent_remaining_budget = (remaining_budget / int(budget)) * 100
 
 # ------------------------------------------
 # OUTPUT
 # ------------------------------------------
-print("Dear " + name)
-print("")
+print(f"Dear {name}")
 print("Your Projected Budget is described below.")
-print("")
-print("Total Budget: " + budget)
-print("Your Apple cost:  " + str(cost_apple) + " " + str(percent_cost_apple))
-print("Your Orange cost:  " + str(cost_orange) + " " + str(percent_cost_orange))
-print("Remaining Budget:  " + str(remaining_budget) + " " + str(percent_remaining_budget))
-print("")
+print(f"Total Budget: {budget}")
+print(f"Your Apple cost:   {apple_total_cost} ({percent_cost_apple:.2f})")
+print(f"Your Orange cost:  {orange_total_cost} ({percent_cost_orange:.2f})")
+print(f"Remaining Budget:  {remaining_budget} ({percent_remaining_budget:.2f})")
